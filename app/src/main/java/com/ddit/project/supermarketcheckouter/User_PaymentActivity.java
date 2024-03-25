b5eb81375b1b5360fed16941e7d606938c2c14fc
Virajv5 <virajgajdharv5@gmail.com>
2024-03-25 14:08:08 +0530

    payment module

--- app/src/main/java/com/ddit/project/supermarketcheckouter/User_PaymentActivity.java
+++ app/src/main/java/com/ddit/project/supermarketcheckouter/User_PaymentActivity.java
@@ -14,6 +14,7 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -149,6 +150,10 @@ public class User_PaymentActivity extends AppCompatActivity {
                 }
                 break;
         }
+
+
+// Add the rest of your code
+
     }
 
     @SuppressLint("MissingPermission")
